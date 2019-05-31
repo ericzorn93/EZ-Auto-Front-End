2da0ba4e0db2161bc792ac29c53b9afa3d7c2681
Eric Zorn <ericzorndesigns@gmail.com>
2019-05-31 07:46:32 -0400

    Created fetch bmw function

--- src/components/bmw/BMW.tsx
+++ src/components/bmw/BMW.tsx
@@ -3,8 +3,13 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import CustomSpinner from "../customSpinner/CustomSpinner";
+import { fetchBmw } from "../../services/api.service";
 
 export class BMW extends Component<any> {
+  componentDidMount() {
+    console.log(this.props);
+  }
+
   componentDidUpdate() {
     console.log(this.props);
   }
@@ -18,7 +23,7 @@ export class BMW extends Component<any> {
 
     return (
       <div>
-        <h1 style={{color: 'red'}}>BMW</h1>
+        <h1 style={{ color: "red" }}>BMW</h1>
       </div>
     );
   }
@@ -28,7 +33,13 @@ const mapStateToProps = (state: any) => ({
   bmwData: state.cars.bmw
 });
 
-const mapDispatchToProps = (dispatch: any) => bindActionCreators({}, dispatch);
+const mapDispatchToProps = (dispatch: any) =>
+  bindActionCreators(
+    {
+      dispatchGetBMWModels: fetchBmw
+    },
+    dispatch
+  );
 
 export default connect(
   mapStateToProps,
